72422bf032aa88181008cbca1fbe198b58974523
jennawlin3 <jenniferacosta437@gmail.com>
2024-09-23 17:09:40 -0400

    updated index.html and styles.css

---
+++ app/app.js
@@ -0,0 +1,19 @@
+
+/*
+            <div class="cart-item">
+              <div class="product-info_container__cart">
+                <img src="./images/image-product-1-thumbnail.jpg" alt="Product thumbnail" srcset="" class="product-pic_cart">
+                <div class="product-info_cart">
+                  <p class="product-title_cart">
+                    Fall Limited Edition Sneakers
+                  </p>
+                  <div class="product-prices_cart">
+                    <p class="individual-price_container__cart">$<span class="individual-price__cart">125.00</span></p>
+                    <p class="product-count_container__cart">x<span class="item-count__cart">3</span></p>
+                    <p class="total-price_container">$<span class="total">375.00</span></p>
+                  </div>
+                </div>
+                <img src="./images/icon-delete.svg" alt="Delete icon" srcset="" class="delete-btn">
+              </div>
+            </div>
+             */
